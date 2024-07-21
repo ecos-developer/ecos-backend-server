6b558d451ef111ee06b85e7bfdbce223e04ddae6
johe lappy <johevin.blesstowi@binus.ac.id>
2024-07-22 02:02:23 +0700

    fix user detail

--- src/api/user/user_detail/user_detail.controller.ts
+++ src/api/user/user_detail/user_detail.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Body, Patch, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  UseGuards,
+  Req,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { UserDetailService } from './user_detail.service';
 import { UserDetailDto } from './dto/user_detail.dto';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -16,24 +25,22 @@ export class UserDetailController {
   @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'get user detail by token' })
   async findOne(@Req() req: Request) {
-    return await this.userDetailService.findOne(req.user as User);
+    const userDetail = await this.userDetailService.findOne(req.user as User);
+    return new HttpException(userDetail, HttpStatus.CREATED);
   }
 
   @Patch('')
   @UseGuards(JwtAuthGuard)
-  // @ApiConsumes('multipart/form-data')
-  // @UsePipes(new ParseUserDetailPipe())
-  // @UseInterceptors(FileInterceptor('profile_image_file'))
   @ApiOperation({ summary: 'update user detail by token (optional field)' })
   @ApiBody({
     description: 'endpoint for customer, driver, and admin detail information',
     type: UserDetailDto,
   })
-  async update(
-    @Req() req: Request,
-    // @UploadedFile() profile_image_file: Express.Multer.File,
-    @Body() userDetailDto: UserDetailDto,
-  ) {
-    return await this.userDetailService.update(req.user as User, userDetailDto);
+  async update(@Req() req: Request, @Body() userDetailDto: UserDetailDto) {
+    const updateUser = await this.userDetailService.update(
+      req.user as User,
+      userDetailDto,
+    );
+    return new HttpException(updateUser, HttpStatus.CREATED);
   }
 }
--- src/api/user/user_detail/user_detail.module.ts
+++ src/api/user/user_detail/user_detail.module.ts
@@ -4,11 +4,10 @@ import { UserDetailController } from './user_detail.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from 'src/api/auth/strategies/jwt.strategy';
-import { EnvService } from 'src/api/env/env.service';
 
 @Module({
   imports: [PrismaModule, PassportModule],
   controllers: [UserDetailController],
-  providers: [UserDetailService, EnvService, JwtStrategy],
+  providers: [UserDetailService, JwtStrategy],
 })
 export class UserDetailModule {}
