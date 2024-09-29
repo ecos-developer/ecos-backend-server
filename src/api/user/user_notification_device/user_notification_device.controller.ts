0d06f51bf7b7419da344bfcdadb4a679849e2c01
johe lappy <johevin.blesstowi@binus.ac.id>
2024-09-29 23:27:41 +0700

    update customer order header

--- src/api/user/user_notification_device/user_notification_device.controller.ts
+++ src/api/user/user_notification_device/user_notification_device.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
+  MethodNotAllowedException,
 } from '@nestjs/common';
 import { UserNotificationDeviceService } from './user_notification_device.service';
 import { CreateUserNotificationDeviceDto } from './dto/create-user_notification_device.dto';
 import { UpdateUserNotificationDeviceDto } from './dto/update-user_notification_device.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('UserNotificationDevices Table (token required, all role authorized)')
 @Controller('user-notification-device')
@@ -21,50 +24,213 @@ export class UserNotificationDeviceController {
 
   @Post()
   @ApiOperation({
-    summary: 'create new notification devices based on jwt token',
+    summary: 'create new notification devices',
   })
   async create(
     @Body() createUserNotificationDeviceDto: CreateUserNotificationDeviceDto,
   ) {
-    return this.userNotificationDeviceService.create(
-      createUserNotificationDeviceDto,
+    const findUserById = await this.userNotificationDeviceService.findUserById(
+      createUserNotificationDeviceDto.user_id,
     );
+    if (!findUserById) {
+      throw new MethodNotAllowedException(
+        `User with user_id ${createUserNotificationDeviceDto.user_id} is not found!`,
+      );
+    }
+
+    const newUserNotificationDevice =
+      await this.userNotificationDeviceService.create(
+        createUserNotificationDeviceDto,
+      );
+
+    return new HttpException(newUserNotificationDevice, HttpStatus.CREATED);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'get all notification devices',
+  })
   async findAll() {
-    return this.userNotificationDeviceService.findAll();
+    const allUserNotificationDevices =
+      await this.userNotificationDeviceService.findAll();
+    return new HttpException(allUserNotificationDevices, HttpStatus.OK);
   }
 
-  @Get(':id')
-  async findOne(@Param('id') user_notification_device_id: string) {
-    return this.userNotificationDeviceService.findOne(
-      user_notification_device_id,
-    );
+  @Get(':user_notification_device_id')
+  @ApiOperation({
+    summary: 'get notification device based its id',
+  })
+  @ApiParam({
+    name: 'user_notification_device_id',
+    description: 'user_notification_device_id for the notification devices',
+    type: String,
+    example: 'mana saya tau id nya',
+  })
+  async findOne(
+    @Param('user_notification_device_id') user_notification_device_id: string,
+  ) {
+    const findUserNotificationDevice =
+      await this.userNotificationDeviceService.findOne(
+        user_notification_device_id,
+      );
+    if (!findUserNotificationDevice) {
+      throw new MethodNotAllowedException(
+        `UserNotificationDevices with id ${user_notification_device_id} is not found!`,
+      );
+    }
+    return new HttpException(findUserNotificationDevice, HttpStatus.OK);
   }
 
   @Get('user/:user_id')
-  async findByUserId(@Param('user_id') user_notification_device_id: string) {
-    return this.userNotificationDeviceService.findOne(
-      user_notification_device_id,
-    );
+  @ApiOperation({
+    summary: 'get notification device based on user_id',
+  })
+  @ApiParam({
+    name: 'user_id',
+    description: 'user_id from table users',
+    type: String,
+    example: 'mana saya tau id nya',
+  })
+  async findByUserId(@Param('user_id') user_id: string) {
+    const findUserById =
+      await this.userNotificationDeviceService.findUserById(user_id);
+    if (!findUserById) {
+      throw new MethodNotAllowedException(
+        `User with user_id ${user_id} is not found!`,
+      );
+    }
+    const findByUserId =
+      await this.userNotificationDeviceService.findByUserId(user_id);
+    if (!findByUserId) {
+      throw new MethodNotAllowedException(
+        `UserNotificationDevices with user_id ${user_id} is not found!`,
+      );
+    }
+    return new HttpException(findByUserId, HttpStatus.OK);
   }
 
-  @Patch(':id')
+  @Patch(':user_notification_device_id')
+  @ApiOperation({
+    summary: 'update notification device based its id',
+  })
+  @ApiParam({
+    name: 'user_notification_device_id',
+    description: 'user_notification_device_id for the notification devices',
+    type: String,
+    example: 'mana saya tau id nya',
+  })
   async update(
-    @Param('id') id: string,
+    @Param('user_notification_device_id') user_notification_device_id: string,
     @Body() updateUserNotificationDeviceDto: UpdateUserNotificationDeviceDto,
   ) {
-    return this.userNotificationDeviceService.update(
-      +id,
-      updateUserNotificationDeviceDto,
-    );
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') user_notification_device_id: string) {
-    return this.userNotificationDeviceService.remove(
+    const findById = await this.userNotificationDeviceService.findOne(
       user_notification_device_id,
     );
+    if (!findById) {
+      throw new MethodNotAllowedException(
+        `UserNotificationDevices with id ${user_notification_device_id} is not found!`,
+      );
+    }
+    const updateById = await this.userNotificationDeviceService.update(
+      user_notification_device_id,
+      updateUserNotificationDeviceDto,
+    );
+    return new HttpException(updateById, HttpStatus.OK);
+  }
+
+  @Get('user-and-token/:user_id/:push_token')
+  @ApiOperation({
+    summary: 'update notification device based on user_id and token',
+  })
+  @ApiParam({
+    name: 'user_id',
+    description: 'user_id for the notification devices',
+    type: String,
+    example: 'mana saya tau user_id nya',
+  })
+  @ApiParam({
+    name: 'push_token',
+    description: 'push_token for the notification devices',
+    type: String,
+    example: 'mana saya tau push_token nya',
+  })
+  async findByIdAndToken(
+    @Param('user_id') user_id: string,
+    @Param('push_token') push_token: string,
+  ) {
+    const findByIdAndToken =
+      await this.userNotificationDeviceService.findByIdAndPushToken(
+        user_id,
+        push_token,
+      );
+    if (!findByIdAndToken) {
+      throw new MethodNotAllowedException(
+        `UserNotificationDevices with user_id ${user_id} and push_token ${push_token} is not found!`,
+      );
+    }
+    return new HttpException(findByIdAndToken, HttpStatus.OK);
+  }
+
+  @Delete('user-and-token/:user_id/:push_token')
+  @ApiOperation({
+    summary: 'update notification device based on user_id and token',
+  })
+  @ApiParam({
+    name: 'user_id',
+    description: 'user_id for the notification devices',
+    type: String,
+    example: 'mana saya tau user_id nya',
+  })
+  @ApiParam({
+    name: 'push_token',
+    description: 'push_token for the notification devices',
+    type: String,
+    example: 'mana saya tau push_token nya',
+  })
+  async removeByIdAndPushToken(
+    @Param('user_id') user_id: string,
+    @Param('push_token') push_token: string,
+  ) {
+    const findByIdAndToken =
+      await this.userNotificationDeviceService.findByIdAndPushToken(
+        user_id,
+        push_token,
+      );
+    if (!findByIdAndToken) {
+      throw new MethodNotAllowedException(
+        `UserNotificationDevices with user_id ${user_id} and push_token ${push_token} is not found!`,
+      );
+    }
+    const deleteById = await this.userNotificationDeviceService.removeById(
+      findByIdAndToken.user_notification_device_id,
+    );
+    return new HttpException(deleteById, HttpStatus.OK);
+  }
+
+  @Delete(':user_notification_device_id')
+  @ApiOperation({
+    summary: 'update notification device based on its id',
+  })
+  @ApiParam({
+    name: 'user_notification_device_id',
+    description: 'user_notification_device_id for the notification devices',
+    type: String,
+    example: 'mana saya tau user_notification_device_id nya',
+  })
+  async removeById(
+    @Param('user_notification_device_id') user_notification_device_id: string,
+  ) {
+    const findByIdAndToken = await this.userNotificationDeviceService.findOne(
+      user_notification_device_id,
+    );
+    if (!findByIdAndToken) {
+      throw new MethodNotAllowedException(
+        `UserNotificationDevices with id ${user_notification_device_id} is not found!`,
+      );
+    }
+    const deleteById = await this.userNotificationDeviceService.removeById(
+      user_notification_device_id,
+    );
+    return new HttpException(deleteById, HttpStatus.OK);
   }
 }
--- src/api/user/user_notification_device/user_notification_device.service.ts
+++ src/api/user/user_notification_device/user_notification_device.service.ts
@@ -1,29 +1,159 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserNotificationDeviceDto } from './dto/create-user_notification_device.dto';
 import { UpdateUserNotificationDeviceDto } from './dto/update-user_notification_device.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserNotificationDeviceService {
-  create(createUserNotificationDeviceDto: CreateUserNotificationDeviceDto) {
-    return 'This action adds a new userNotificationDevice';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(
+    createUserNotificationDeviceDto: CreateUserNotificationDeviceDto,
+  ) {
+    const newUserNotif = await this.prisma.userNotificationDevices.create({
+      data: {
+        ...createUserNotificationDeviceDto,
+      },
+      include: {
+        user: {
+          include: {
+            user_detail: true,
+            driver_detail: true,
+            customer_detail: true,
+          },
+        },
+      },
+    });
+    return newUserNotif;
   }
 
-  findAll() {
-    return `This action returns all userNotificationDevice`;
+  async findAll() {
+    const allUserNotif = await this.prisma.userNotificationDevices.findMany({
+      include: {
+        user: {
+          include: {
+            user_detail: true,
+            driver_detail: true,
+            customer_detail: true,
+          },
+        },
+      },
+    });
+    return allUserNotif;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} userNotificationDevice`;
+  async findOne(user_notification_device_id: string) {
+    const findUserNotifById =
+      await this.prisma.userNotificationDevices.findFirst({
+        where: {
+          user_notification_device_id,
+        },
+        include: {
+          user: {
+            include: {
+              user_detail: true,
+              driver_detail: true,
+              customer_detail: true,
+            },
+          },
+        },
+      });
+    return findUserNotifById;
   }
 
-  update(
-    id: number,
+  async findUserById(user_id: string) {
+    const findUser = await this.prisma.user.findFirst({
+      where: {
+        user_id,
+      },
+    });
+    return findUser;
+  }
+
+  async findByUserId(user_id: string) {
+    const findUserNotifByUserId =
+      await this.prisma.userNotificationDevices.findMany({
+        where: {
+          user: {
+            user_id,
+          },
+        },
+        include: {
+          user: {
+            include: {
+              user_detail: true,
+              driver_detail: true,
+              customer_detail: true,
+            },
+          },
+        },
+      });
+    return findUserNotifByUserId;
+  }
+
+  async update(
+    user_notification_device_id: string,
     updateUserNotificationDeviceDto: UpdateUserNotificationDeviceDto,
   ) {
-    return `This action updates a #${id} userNotificationDevice`;
+    const updateUserNotifById =
+      await this.prisma.userNotificationDevices.update({
+        where: {
+          user_notification_device_id,
+        },
+        data: {
+          ...updateUserNotificationDeviceDto,
+        },
+        include: {
+          user: {
+            include: {
+              user_detail: true,
+              driver_detail: true,
+              customer_detail: true,
+            },
+          },
+        },
+      });
+    return updateUserNotifById;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} userNotificationDevice`;
+  async findByIdAndPushToken(user_id: string, push_token: string) {
+    const findByIdandToken =
+      await this.prisma.userNotificationDevices.findFirst({
+        where: {
+          user_id,
+          AND: {
+            push_token,
+          },
+        },
+        include: {
+          user: {
+            include: {
+              user_detail: true,
+              driver_detail: true,
+              customer_detail: true,
+            },
+          },
+        },
+      });
+    return findByIdandToken;
+  }
+
+  async removeById(user_notification_device_id: string) {
+    const deleteUserNotifById =
+      await this.prisma.userNotificationDevices.delete({
+        where: {
+          user_notification_device_id,
+        },
+        include: {
+          user: {
+            include: {
+              user_detail: true,
+              driver_detail: true,
+              customer_detail: true,
+            },
+          },
+        },
+      });
+    return deleteUserNotifById;
   }
 }
